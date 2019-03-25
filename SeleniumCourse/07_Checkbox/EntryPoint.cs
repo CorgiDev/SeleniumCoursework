34d2cc90c79a0a243f305daf4fd45f35aebd4a17
Beth Gray <9934135+CorgiDev@users.noreply.github.com>
2019-03-24 22:35:23 -0400

    Started with the checkbox project.

--- SeleniumCourse/07_Checkbox/EntryPoint.cs
+++ SeleniumCourse/07_Checkbox/EntryPoint.cs
@@ -1,6 +1,18 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 class EntryPoint
 {
+    static IWebDriver driver = new ChromeDriver();
+
+    private static IWebElement checkbox;
+
     static void Main()
     {
+        string url = "";
+
+        driver.Navigate().GoToUrl(url);
+
+        driver.Quit();
     }
 }
