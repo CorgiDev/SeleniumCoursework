93c72ecc8291cdac5728d8d91ae57a003f0ac308
Beth Gray <9934135+CorgiDev@users.noreply.github.com>
2019-04-13 12:56:26 -0400

    Set up initial structure of radio button selection test.

--- SeleniumCourse/08_RadioButton/EntryPoint.cs
+++ SeleniumCourse/08_RadioButton/EntryPoint.cs
@@ -1,6 +1,22 @@
+using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 class EntryPoint
 {
+    static IWebDriver driver = new ChromeDriver();
+
+    static IWebElement checkBox;
+
     static void Main()
     {
+        string url = "http://testing.todorvachev.com/special-elements/radio-button-test/";
+
+        string option = "1";
+
+        driver.Navigate().GoToUrl(url);
+
+        driver.Quit();
     }
 }
