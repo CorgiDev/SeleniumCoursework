04fa6cea56aa7ef5a571fd76b75aa614c3a0ea2f
Beth Gray <9934135+CorgiDev@users.noreply.github.com>
2019-04-13 13:57:43 -0400

    Set up the DropDown and AlertBox projects for when I start that parts of the course.

--- SeleniumCourse/09_DropDownMenu/EntryPoint.cs
+++ SeleniumCourse/09_DropDownMenu/EntryPoint.cs
@@ -1,6 +1,17 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 class EntryPoint
 {
+    static IWebDriver driver = new ChromeDriver();
+    private static IWebElement dropDownMenu;
+
     static void Main()
     {
+        string url = "http://testing.todvachev.com/special-elements/drop-down-menu-test/";
+
+        driver.Navigate().GoToUrl(url);
+
+        driver.Quit();
     }
 }
