4dcb2fae717381645586fad8e91b296eb6f0d88b
Elizabeth Gray <egray@corgidev.com>
2019-04-15 00:52:57 -0400

    Practiced making and saving screenshots with Selenium.

---
+++ SeleniumCourse/SeleniumScreenshot/EntryPoint.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver chrome = new ChromeDriver();
+        string screenshotsDirectory = Directory.GetCurrentDirectory() + @"\screenshots\";
+
+        chrome.Navigate().GoToUrl("http://google.com");
+
+        Screenshot googleScreenshot = ((ITakesScreenshot)chrome).GetScreenshot();
+
+        if (!Directory.Exists(screenshotsDirectory))
+        {
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\screenshots\");
+        }
+
+        googleScreenshot.SaveAsFile(Directory.GetCurrentDirectory() + @"\screenshots\googlescreenshot2.png", ScreenshotImageFormat.Png);
+
+        chrome.Quit();
+    }
+}
