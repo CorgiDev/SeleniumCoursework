e2faf856e814bfcc6f5d5157514106659a5241ff
Elizabeth Gray <egray@corgidev.com>
2019-04-16 23:37:52 -0400

    Learned baout using different webdrivers for different browsers.

---
+++ SeleniumCourse/DifferentBrowsers/EntryPoint.cs
@@ -0,0 +1,16 @@
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Opera;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver fireDriver = new FirefoxDriver();
+        IWebDriver operaDriver = new OperaDriver();
+
+        fireDriver.Navigate().GoToUrl("http://google.com");
+        operaDriver.Navigate().GoToUrl("http://google.com");
+    }
+}
