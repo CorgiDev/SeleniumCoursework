df30d07882993771cbc4e89fee37711f81aa3bca
Elizabeth Gray <egray@corgidev.com>
2019-04-16 22:48:46 -0400

    Created a method to drag one element left, right, up, or down. Used it to drag one element down enough that it swaps places with the one below it.

--- SeleniumCourse/01_UserlikeActions/EntryPoint.cs
+++ SeleniumCourse/01_UserlikeActions/EntryPoint.cs
@@ -31,11 +31,28 @@ class EntryPoint
 
         Actions actions = new Actions(driver);
 
-        actions.MoveToElement(elements[0]).Build().Perform();
-
+        ///////////////////////////////////////////
+        //Checks if the color of the 1st element
+        //is the correct color while being hovered
+        //over and checks the second to make sure
+        //it is different.
+        ///////////////////////////////////////////
         //string lightGreen = "rgba(144, 238, 144, 1)";
         //actions.MoveToElement(elements[0]).Build().Perform();
         //Console.WriteLine(elements[0].GetCssValue("background-color") == lightGreen);
         //Console.WriteLine(elements[1].GetCssValue("background-color") == lightGreen);
+
+        //Drags the first element down to the location of the second element and then releases it.
+        MoveElement(actions, elements[0], elements[1], 0, 10);
+    }
+
+    static void MoveElement(Actions actions, IWebElement from, IWebElement to, int x = 0, int y = 0)
+    {
+        actions.ClickAndHold(from)
+            .MoveToElement(to)
+            .MoveByOffset(x , y)
+            .Release()
+            .Build()
+            .Perform();
     }
 }
