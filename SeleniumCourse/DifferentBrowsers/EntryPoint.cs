a766d64e3945a1217269d013f7bd40663378eecf
Elizabeth Gray <egray@corgidev.com>
2019-04-17 21:59:21 -0400

    Added a use of the Internet Explorer webdriver to my DifferentBrowsers project.

--- SeleniumCourse/DifferentBrowsers/EntryPoint.cs
+++ SeleniumCourse/DifferentBrowsers/EntryPoint.cs
@@ -1,6 +1,7 @@
 using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Opera;
 
 class EntryPoint
@@ -8,9 +9,11 @@ class EntryPoint
     static void Main()
     {
         //IWebDriver fireDriver = new FirefoxDriver();
-        IWebDriver operaDriver = new OperaDriver();
+        //IWebDriver operaDriver = new OperaDriver();
+        IWebDriver ieDriver = new InternetExplorerDriver();
 
         //fireDriver.Navigate().GoToUrl("http://google.com");
-        operaDriver.Navigate().GoToUrl("http://google.com");
+        //operaDriver.Navigate().GoToUrl("http://google.com");
+        ieDriver.Navigate().GoToUrl("http://google.com");
     }
 }
