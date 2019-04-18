99d203b3ed473b7b8061743f6a6e3d7ff54df9a0
Elizabeth Gray <egray@corgidev.com>
2019-04-18 17:39:56 -0400

    Finished course section on opening, switching to, and closing tabs.

--- SeleniumCourse/OpenCloseTabs/EntryPoint.cs
+++ SeleniumCourse/OpenCloseTabs/EntryPoint.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Interactions;
 
@@ -11,12 +12,36 @@ class Program
 {
     static void Main()
     {
-        IWebDriver driver = new InternetExplorerDriver();
+        //IWebDriver driver = new InternetExplorerDriver();
+        IWebDriver driver = new ChromeDriver();
 
         driver.Navigate().GoToUrl("http://google.com");
 
         Actions actions = new Actions(driver);
 
         actions.KeyDown(Keys.Control).SendKeys("t").Build().Perform();
+
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+        ((IJavaScriptExecutor)driver).ExecuteScript("window.open();");
+
+        List<string> handles = driver.WindowHandles.ToList();
+
+        driver.SwitchTo().Window(handles[2]);
+        driver.Navigate().GoToUrl("http://gmail.com");
+
+        for (int i = 0; i < handles.Count; i++)
+        {
+            if (i != 2)
+            {
+                driver.SwitchTo().Window(handles[i]);
+                driver.Close();
+            }
+        }
     }
 }
