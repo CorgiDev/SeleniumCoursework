3aca49cf7d7132b1cb38788c5cc02900b99767db
Elizabeth Gray <egray@corgidev.com>
2019-04-18 17:26:45 -0400

    Made my code open IE and open a new tab.

--- SeleniumCourse/OpenCloseTabs/EntryPoint.cs
+++ SeleniumCourse/OpenCloseTabs/EntryPoint.cs
@@ -5,11 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Interactions;
 
 class Program
 {
     static void Main()
     {
         IWebDriver driver = new InternetExplorerDriver();
+
+        driver.Navigate().GoToUrl("http://google.com");
+
+        Actions actions = new Actions(driver);
+
+        actions.KeyDown(Keys.Control).SendKeys("t").Build().Perform();
     }
 }
