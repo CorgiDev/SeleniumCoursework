fdb74ff779d3c954283f05b44d2d1fca25d8a243
Elizabeth Gray <egray@corgidev.com>
2019-04-20 00:52:54 -0400

    Investigating why my Add() method currently does not work.

--- SeleniumCourse/PhantomProject/EntryPoint.cs
+++ SeleniumCourse/PhantomProject/EntryPoint.cs
@@ -1,27 +1,41 @@
 using System;
 using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System.Collections.Generic; //Needed for the list below.
+using System.IO;
+using System.Threading;
 
-namespace PhantomJS
+class EntryPoint
 {
-    class Program
+    static void Main()
     {
-        static void Main()
+        List<string> extractedLinks = new List<string>();
+
+        //Headless option for ChromeDriver
+        ChromeOptions options = new ChromeOptions();
+        options.AddArgument("--headless");
+
+        //ChromeDriver
+        IWebDriver driver = new ChromeDriver(options);
+
+        string sitemapURL = "http://testing.todvachev.com/sitemap-posttype-post.xml";
+        string[] pageSource;
+
+        driver.Navigate().GoToUrl(sitemapURL);
+
+        pageSource = driver.PageSource.Split(' ');
+
+        foreach (var item in pageSource)
         {
-            //Headless option for ChromeDriver
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("--headless");
-
-            //ChromeDriver
-            IWebDriver driver = new ChromeDriver(options);
-
-            string sitemapURL = "http://testing.todvachev.com/sitemap-posttype-post.xml";
-            string[] pageSource;
-
-            driver.Navigate().GoToUrl(sitemapURL);
-
-            pageSource = driver.PageSource.Split(' ');
+            if (item.Contains("<loc>"))
+            {
+                extractedLinks.Add(item);
+                Console.WriteLine(item);
+            }
         }
     }
 }
+
 
