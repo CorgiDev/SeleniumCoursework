b7caf734c5fddefb843352d470b725b44425ac2e
Elizabeth Gray <egray@corgidev.com>
2019-04-22 13:10:32 -0400

    Started working on setting up my mobile testing environment. Pushed what little I had on up so I could switch computers.

---
+++ SeleniumCourse/AutoTest_B/Driver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTest_B
+{
+    class Driver
+    {
+        public static AppiumDriver<IWebElement> driver { get; set; }
+
+        public static void WaitForElementUpTo(int seconds = 5)
+        {
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(seconds));
+        }
+    }
+}
